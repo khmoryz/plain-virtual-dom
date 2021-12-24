f6021201947ab2a765c1fc0ef85bdc3d8db9821e
kokkenro0520@gmail.com <kokkenro0520@gmail.com>
2021-12-24 21:56:00 +0900

    update: 子要素を配列化する

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const messageObj = {
   tagName: "p",
-  children: "this is virtual dom",
+  children: ["this is virtual dom"],
 };
 
 render(messageObj);
 
 function render(obj) {
   const pElement = document.createElement(obj.tagName);
-  const textNode = document.createTextNode(messageObj.children);
+  const textNode = document.createTextNode(messageObj.children[0]);
   pElement.appendChild(textNode);
   document.body.appendChild(pElement);
 }
