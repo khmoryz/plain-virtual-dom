54bc196bb0c0c73531a1da840d91bbe842343bc9
kokkenro0520@gmail.com <kokkenro0520@gmail.com>
2021-12-27 15:49:39 +0900

    update: タグ変更をパッチ可能にする

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const messageObj = {
+const firstObj = {
   tagName: "div",
   attributes: { id: "div-id" },
   children: [
@@ -15,9 +15,58 @@ const messageObj = {
   ],
 };
 
-render(messageObj);
+// ブラウザ表示中のオブジェクト
+let currentObj;
+render(firstObj);
+
+const seccondObj = {
+  tagName: "p",
+  attributes: { id: "div-id" },
+  children: [
+    {
+      tagName: "p",
+      attributes: { id: "p-id" },
+      children: [
+        {
+          tagName: "text",
+          attributes: { content: "this is virtual dom" },
+        },
+      ],
+    },
+  ],
+};
+
+const changes = diff(currentObj, seccondObj);
+patch(changes);
+
+function diff(oldObj, newObj) {
+  if (oldObj.tagName != newObj.tagName) {
+    const id = oldObj.attributes.id;
+    const tagName = newObj.tagName;
+    return {
+      id: oldObj.attributes.id,
+      type: "tagName",
+      value: newObj.tagName,
+    };
+  }
+}
+
+function patch(target) {
+  switch (target.type) {
+    case "tagName":
+      const targetElement = window.document.getElementById(target.id);
+      const newElement = document.createElement(target.value);
+
+      targetElement.replaceWith(newElement);
+      break;
+    default:
+      console.error("invalid patch type");
+      break;
+  }
+}
 
 function render(obj) {
+  currentObj = obj;
   const realDOM = genRealDOM(obj);
   document.body.appendChild(realDOM);
 }
